0d349c10935415896ff930a757e599a450647580
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-27 16:54:36 +0100

    Add const to convert reverse array in string and add an if-else to compare the two words and determinate if user name is palindrome or not

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,30 @@ console.log('Pari o dispari?');
 //         - Usare .split per scomporre la parola in caratteri
 //         - Creare un secondo array coi caratteri della parola inserita al contrario con .reverse
 //             - .join per creare il secondo array
-    
+
 //     - Verificare se la parola è palindroma
 //         - SE i caretteri della parola e i caratteri invertiti sono uguali la parola è pallindroma
 //             - Mssaggio appropriato
 //         - ALTRIMENTI la parola non è palindroma
 //             - Messaggio appropriato
 
-// const userWord = prompt("Inserisci una parola per scoprire se è palindroma")
+const userWord = prompt("Inserisci una parola per scoprire se è palindroma")
 // console.log(userWord);
 
-const userWord = "pera";
+// const userWord = "pera";
 
 const digit = userWord.split("");
 console.log(digit);
 
 const digitBackward = digit.toReversed();
-
 console.log(digitBackward);
-// let digitBackwards = []
-// console.log(digitBackwards);
+
+const reversWord = digitBackward.join("")
+console.log(reversWord);
+
+if (userWord === reversWord) {
+    console.log(`The word "${userWord}" is a palindrome`);
+} else {
+    console.log(`The word "${userWord}" is not a palindrome`);
+}
+
