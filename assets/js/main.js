8e4fc0eee1d2f8126df51077e6d776ebc57548c0
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-27 15:20:20 +0100

    Add variable to collect the user word with promp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 console.log('Pari o dispari?');
+
+// - Step 1: Chiedere all’utente di inserire una parola 
+//     - Creare una variabile per la parola dell'utente
+//         - Usare prompt per l'inserimento
+
+//      - Creare un array contenente i caratteri della parola inserita
+//         - Usare .split per scomporre la parola in caratteri
+//         - Creare un secondo array coi caratteri della parola inserita al contrario con .reverse
+//             - .join per creare il secondo array
+    
+//     - Verificare se la parola è palindroma
+//         - SE i caretteri della parola e i caratteri invertiti sono uguali la parola è pallindroma
+//             - Mssaggio appropriato
+//         - ALTRIMENTI la parola non è palindroma
+//             - Messaggio appropriato
+
+const userWord = prompt("Inserisci una parola per scoprire se è palindroma")
+
+console.log(userWord);
