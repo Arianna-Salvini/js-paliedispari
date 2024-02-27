dec59745b116981bfb6d996ecd879ae7291a20d5
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-27 18:05:25 +0100

    Fix code with a function to make the game

--- Pari o dispari/assets/js/main.js
+++ Pari o dispari/assets/js/main.js
@@ -2,21 +2,27 @@ console.log("Pari o dispari?");
 const userChoice = prompt('Pari o dispari').toLocaleLowerCase()
 const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
 
-const pcNumber = Math.round(Math.random() *5) +1
+const pcNumber = Math.round(Math.random() * 5) + 1
 
 console.log(userChoice);
 console.log(userNumber);
 console.log(pcNumber);
 
-let sum= userNumber + pcNumber
+let sum = userNumber + pcNumber
 
 console.log(sum);
 
-if (sum % 2 == 0 && userChoice == 'pari') {
-    console.log('you win');
-    
-} else if ( sum %2 != 0 && userChoice == 'dispari') {
-    console.log('win');
-} else {
-    console.log('loose');
+let message = ""
+
+function gameEvenOrOdd(userChoice, userNumber) {
+    if (sum % 2 == 0 && userChoice == 'pari') {
+        return message = 'You win!'
+    } else if (sum % 2 != 0 && userChoice == 'dispari') {
+        return message = 'You win!';
+    } else {
+        return message = 'You lost!';
+    }
+
 }
+
+alert(gameEvenOrOdd(userChoice, userNumber))
